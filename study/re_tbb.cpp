de04071ed6669b9d0821d646ce0263ba3145406b
hhunity <131772276+hhunity@users.noreply.github.com>
2026-01-10 06:40:37 +0900

    re_tbb.cpp を作成

---
+++ study/re_tbb.cpp
@@ -0,0 +1,61 @@
+class Camera {
+public:
+    void start();
+    void stop();
+    // grab(), registerCallback() etc...
+};
+
+class ImageProcessor {
+public:
+    void start();
+    void stop();
+    // process(frame) etc...
+};
+
+// “所有しない” パイプライン：部品は参照で受ける
+class Pipeline {
+public:
+    Pipeline(Camera& cam, ImageProcessor& proc)
+        : cam_(cam), proc_(proc) {}
+
+    void start_full() {
+        cam_.start();
+        proc_.start();
+        // tbb task group start etc...
+    }
+
+    void stop_full() {
+        // stop tasks first, then proc, then cam
+        proc_.stop();
+        cam_.stop();
+    }
+
+private:
+    Camera& cam_;
+    ImageProcessor& proc_;
+};
+
+class AppController {
+public:
+    AppController()
+      : cam_(std::make_unique<Camera>()),
+        proc_(std::make_unique<ImageProcessor>()),
+        pipeline_(std::make_unique<Pipeline>(*cam_, *proc_)) {}
+
+    void start_camera_only() {
+        cam_->start();
+    }
+
+    void start_everything() {
+        pipeline_->start_full();
+    }
+
+    void stop_all() {
+        pipeline_->stop_full();
+    }
+
+private:
+    std::unique_ptr<Camera> cam_;
+    std::unique_ptr<ImageProcessor> proc_;
+    std::unique_ptr<Pipeline> pipeline_;
+};
